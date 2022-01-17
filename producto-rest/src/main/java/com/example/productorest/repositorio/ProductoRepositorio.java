0ac6716117b1b5e663f044281ce27bbf40817d8c
Oscar Jesus Sanabria Tavera <52457473+SanabriaOscar@users.noreply.github.com>
2022-01-17 15:27:06 -0500

    api rest with  posmant

---
+++ producto-rest/src/main/java/com/example/productorest/repositorio/ProductoRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.productorest.repositorio;
+
+import com.example.productorest.modelo.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository <Producto, Integer> {
+}
